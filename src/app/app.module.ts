acf45a91ee09f79ef32ecdd856c11a8c0fd6ba5d
Gustavo Coelho <gutorc@hotmail.com>
2023-03-20 14:57:09 -0300

    feat: initial
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './users/users.component';
+import { UsersDetailComponent } from './users-detail/users-detail.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MaterialExampleModule} from '../material.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UsersDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,96 @@
+import { Component, ViewChild, AfterViewInit} from '@angular/core';
+import { User } from '../user';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {HttpClient} from '@angular/common/http';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort, SortDirection} from '@angular/material/sort';
+import {merge, Observable, of as observableOf} from 'rxjs';
+import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'lastName'];
+  exampleDatabase: ExampleHttpDatabase | null;
+  data: User[] = [];
+
+  resultsLength = 0;
+  isLoadingResults = true;
+  isRateLimitReached = false;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private _httpClient: HttpClient) {}
+
+  ngAfterViewInit() {
+    this.exampleDatabase = new ExampleHttpDatabase(this._httpClient);
+
+    // If the user changes the sort order, reset back to the first page.
+    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+
+    merge(this.sort.sortChange, this.paginator.page)
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this.exampleDatabase!.getRepoIssues(
+            this.sort.active,
+            this.sort.direction,
+            this.paginator.pageIndex,
+          ).pipe(catchError(() => {
+            console.log('error') 
+            return observableOf(null)
+          }));
+        }),
+        map(data => {
+          // Flip flag to show that loading has finished.
+          this.isLoadingResults = false;
+          this.isRateLimitReached = data === null;
+          console.log('data', data)
+          if (data === null) {
+            return [];
+          }
+
+          // Only refresh the result length if there is new data. In case of rate
+          // limit errors, we do not want to reset the paginator to zero, as that
+          // would prevent users from re-triggering requests.
+          this.resultsLength = data.total;
+          return data.items;
+        }),
+      )
+      .subscribe(data => (this.data = data));
+  }
+}
+
+export interface BackendApi {
+  items: User[];
+  total: number;
+}
+
+export class ExampleHttpDatabase {
+  constructor(private _httpClient: HttpClient) {}
+
+  getRepoIssues(sort: string, order: SortDirection, page: number): Observable<BackendApi> {
+    const href = 'http://localhost:3000/users';
+    const requestUrl = `${href}?offset=0&limit=10&sort=${sort}&order=${order}&page=${
+      page + 1
+    }`;
+    console.log('passou aqui', requestUrl)
+    return this._httpClient.get<BackendApi>(requestUrl);
+  }
+}
+
+/** An example database that the data source uses to retrieve data for the table. */
+
+// export class UsersComponent {
+//   user: User = {
+//     id: 1,
+//     name: 'Teste',
+//     lastName: 'Testando',
+//     email: 'testando@teste.com'
+//   }
+// }
